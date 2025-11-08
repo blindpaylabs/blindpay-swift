91a92ca8dd054c504cf43daa0ecbe95970259eb2
Eric Viana <ericvsdefault@gmail.com>
2025-11-08 11:28:57 -0300

    feat: blockchain wallets (#6)

---
+++ Sources/blindpay-swift/Services/ReceiversService.swift
@@ -0,0 +1,27 @@
+//
+//  ReceiversService.swift
+//  blindpay-swift
+//
+//  Created by Eric Viana on 05/11/25.
+//
+
+import Foundation
+
+/// Service for managing receiver resources
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+public final class ReceiversService: Sendable {
+    private let apiClient: APIClient
+    private let instanceId: String
+    private let receiverId: String
+    
+    /// Blockchain wallets management service
+    public let blockchainWallets: BlockchainWalletsService
+    
+    init(apiClient: APIClient, instanceId: String, receiverId: String) {
+        self.apiClient = apiClient
+        self.instanceId = instanceId
+        self.receiverId = receiverId
+        self.blockchainWallets = BlockchainWalletsService(apiClient: apiClient, instanceId: instanceId, receiverId: receiverId)
+    }
+}
+
